377f04e4d13ef4b3582af5265b9c8f7b02f85a3f
Hồ Huy Hoàng <hoangho1147@gmail.com>
2021-10-12 22:26:49 +0700

    fixx eslint

--- client/src/theme/index.js
+++ client/src/theme/index.js
@@ -1,44 +1,38 @@
-import PropTypes from "prop-types";
-import { useMemo } from "react";
+import PropTypes from 'prop-types';
+import { useMemo } from 'react';
 // material
-import { CssBaseline } from "@material-ui/core";
-import {
-  ThemeProvider,
-  createTheme,
-  StyledEngineProvider,
-} from "@material-ui/core/styles";
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider, createTheme, StyledEngineProvider } from '@material-ui/core/styles';
 // hooks
-import useSettings from "../hooks/useSettings";
+import useSettings from '../hooks/useSettings';
 //
-import shape from "./shape";
-import palette from "./palette";
-import typography from "./typography";
-import breakpoints from "./breakpoints";
-import GlobalStyles from "./globalStyles";
-import componentsOverride from "./overrides";
-import shadows, { customShadows } from "./shadows";
+import shape from './shape';
+import palette from './palette';
+import typography from './typography';
+import breakpoints from './breakpoints';
+import GlobalStyles from './globalStyles';
+import componentsOverride from './overrides';
+import shadows, { customShadows } from './shadows';
 
 // ----------------------------------------------------------------------
 
 ThemeConfig.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 
 export default function ThemeConfig({ children }) {
   const { themeMode, themeDirection } = useSettings();
-  const isLight = themeMode === "light";
+  const isLight = themeMode === 'light';
 
   const themeOptions = useMemo(
     () => ({
-      palette: isLight
-        ? { ...palette.light, mode: "light" }
-        : { ...palette.dark, mode: "dark" },
+      palette: isLight ? { ...palette.light, mode: 'light' } : { ...palette.dark, mode: 'dark' },
       shape,
       typography,
       breakpoints,
       direction: themeDirection,
       shadows: isLight ? shadows.light : shadows.dark,
-      customShadows: isLight ? customShadows.light : customShadows.dark,
+      customShadows: isLight ? customShadows.light : customShadows.dark
     }),
     [isLight, themeDirection]
   );
