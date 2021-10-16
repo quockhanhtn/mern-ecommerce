e761e9bccbfa7938814a8b108fe54512c302b41d
Hồ Huy Hoàng <hoangho1147@gmail.com>
2021-10-17 03:13:56 +0700

    add unit test for mocha

---
+++ server/run-test.js
@@ -0,0 +1,7 @@
+// Test for file test.test.js
+import test from './test/test.test.js';
+test();
+
+
+
+
---
+++ server/test/test.test.js
@@ -0,0 +1,10 @@
+import * as assert from 'assert';
+export default function test(){
+  describe('Array', function() {
+    describe('#indexOf()', function() {
+      it('should return -1 when the value is not present', function() {
+        assert.equal([1, 2, 3].indexOf(4), -1);
+      });
+    });
+  });
+}
