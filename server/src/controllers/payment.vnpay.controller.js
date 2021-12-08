e4355b87a2ac3160baa4cda9e1722e06804534f2
Khanh Lam <quockhanhdev@gmail.com>
2021-12-08 21:42:20 +0700

    Update library

--- server/src/controllers/payment.vnpay.controller.js
+++ server/src/controllers/payment.vnpay.controller.js
@@ -1,28 +1,23 @@
 import crypto from 'crypto';
-import * as querystring from 'querystring';
-import config from 'config';
+import queryString from 'query-string';
 import dateFormat from 'dateformat';
 
 export const createPaymentVnPay = async (req, res, next) => {
   try {
-    const ipAddr = req.headers['x-forwarded-for'] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress;
-  
-  
+    const ipAddr = req.ip;
+
     const tmnCode = 'QL509E3K';
     const secretKey = 'OVOALYVDSIREXCICBCAANMFTAEDVNNCV';
     let vnpUrl = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html';
     const returnUrl = 'http://localhost:8888/order/vnpay_return';
-  
+
     const date = new Date();
-  
+
     const createDate = dateFormat(date, 'yyyymmddHHmmss');
     const orderId = dateFormat(date, 'HHmmss');
     const amount = req.body.amount;
     const bankCode = req.body.bankCode;
-  
+
     const orderInfo = req.body.orderDescription;
     const orderType = req.body.orderType;
     let locale = req.body.language;
@@ -47,14 +42,14 @@ export const createPaymentVnPay = async (req, res, next) => {
     if (bankCode !== null && bankCode !== '') {
       vnp_Params['vnp_BankCode'] = bankCode;
     }
-  
+
     vnp_Params = sortObject(vnp_Params);
-    
-    const signData = querystring.stringify(vnp_Params, {encode: false});
+
+    const signData = queryString.stringify(vnp_Params, { encode: false });
     const hmac = crypto.createHmac("sha512", secretKey);
     const signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");
     vnp_Params['vnp_SecureHash'] = signed;
-    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+    vnpUrl += '?' + queryString.stringify(vnp_Params, { encode: false });
     console.log(vnpUrl);
     res.redirect(vnpUrl)
   } catch (err) {
