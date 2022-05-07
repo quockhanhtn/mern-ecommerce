df7d51df2828f0bb300b7e85ec2d4508d1a614a6
Khanh Lam <quockhanhdev@gmail.com>
2022-05-07 21:25:14 +0700

    Update

--- mobile/lib/screens/splash_screen.dart
+++ mobile/lib/screens/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:hk_mobile/core/utils/preference_util.dart';
+import 'package:hk_mobile/screens/home/home_screen.dart';
 import 'package:hk_mobile/screens/intro/intro_screen.dart';
 import 'package:hk_mobile/screens/main/main_screen.dart';
 import 'package:hk_mobile/size_config.dart';
@@ -16,11 +18,16 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> with AfterLayoutMixin<SplashScreen> {
+  //WebSocketChannel channel = WebSocketChannel.connect(Uri.parse(URL));
+  TextEditingController controller = TextEditingController();
+  var sub;
+  late String text;
+
   Future checkFirstSeen() async {
     bool _seen = PreferenceUtil.getBool('seen', false);
 
     if (_seen) {
-      Get.to(const MainScreen());
+      Get.to(const HomeScreen());
       //Navigator.pushNamed(context, HomeScreen.routeName);
     } else {
       Get.to(const IntroScreen());
@@ -30,6 +37,43 @@ class _SplashScreenState extends State<SplashScreen> with AfterLayoutMixin<Splas
 
   @override
   void afterFirstLayout(BuildContext context) => checkFirstSeen();
+
+  @override
+  void initState() {
+    super.initState();
+
+    FlutterLocalNotificationsPlugin notifications = FlutterLocalNotificationsPlugin();
+    const androidInit = AndroidInitializationSettings('app_icon');
+    const iOSInit = IOSInitializationSettings();
+    const init = InitializationSettings(
+      android: androidInit,
+      iOS: iOSInit,
+    );
+
+    notifications.initialize(init).then((done) {
+      // sub = channel.stream.listen((newData) {
+      //   setState(() {
+      //     text = newData;
+      //   });
+
+      notifications.show(
+          0,
+          "New announcement",
+          "asd data",
+          const NotificationDetails(
+            android: AndroidNotificationDetails("announcement_app_0", "Announcement App"),
+            iOS: IOSNotificationDetails(),
+          ));
+      //});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    //channel.sink.close(statusCodes.goingAway);
+    sub.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
