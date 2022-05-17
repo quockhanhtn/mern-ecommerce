e8927c8c7f862cb36d6e893c2ad3b67e82add804
Hồ Huy Hoàng <hoangho1147@gmail.com>
2022-05-17 23:50:20 +0700

    add refactor dialog search

--- client/src/layouts/main/SearchBar.jsx
+++ client/src/layouts/main/SearchBar.jsx
@@ -5,17 +5,14 @@ import searchFill from '@iconify/icons-eva/search-fill';
 // material
 import {
   Box,
-  Button,
-  ClickAwayListener,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
   Grid,
-  Link,
   Paper,
   Typography,
-  OutlinedInput
+  InputAdornment,
+  Autocomplete,
+  TextField,
+  Link,
+  ClickAwayListener
 } from '@material-ui/core';
 // hook
 import { useNavigate } from 'react-router-dom';
@@ -24,9 +21,8 @@ import useLocalStorage from '../../hooks/useLocalStorage';
 //
 import { getSearchSuggest } from '../../api';
 // components
-import { MIconButton, MCircularProgress } from '../../components/@material-extend';
+import { MCircularProgress } from '../../components/@material-extend';
 import { ThumbImgStyle } from '../../components/@styled';
-import AutoFocusTextField from '../../components/AutoFocusTextField';
 // utils
 import { fCurrency } from '../../utils/formatNumber';
 import Label from '../../components/Label';
@@ -36,7 +32,7 @@ import Label from '../../components/Label';
 export default function SearchBar({ iconSx }) {
   const { t, currentLang } = useLocales();
   const navigate = useNavigate();
-  const [isOpen, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(true);
   const [keyWord, setKeyWord] = useState('');
   const [searchHistory, setSearchHistory] = useLocalStorage('searchHistory', []);
 
@@ -96,18 +92,17 @@ export default function SearchBar({ iconSx }) {
     const { price, marketPrice } = product.variants[0];
     const discountPercent = Math.ceil(((marketPrice - price) * 100) / marketPrice);
 
+    if (isLoading) {
+      return <MCircularProgress size={15} sx={{ my: 3 }} />;
+    }
+
     return (
-      <Grid
-        key={product._id}
-        item
-        sm={12}
-        md={6}
-        onClick={() => handleOnClickResultItem(product)}
-        sx={{ cursor: 'pointer' }}
-      >
+      <Grid key={product._id} item onClick={() => handleOnClickResultItem(product)} sx={{ cursor: 'pointer' }}>
         <Paper
           sx={{
             display: 'flex',
+            marginLeft: 3,
+            marginBottom: 2,
             flexDirection: 'row',
             '&:hover': {
               boxShadow: (theme) => theme.customShadows.z8,
@@ -156,86 +151,59 @@ export default function SearchBar({ iconSx }) {
 
   return (
     <ClickAwayListener onClickAway={handleClose}>
-      <div>
-        {!isOpen && (
-          <MIconButton onClick={handleOpen} sx={iconSx}>
-            <Icon icon={searchFill} width={20} height={20} />
-          </MIconButton>
-        )}
-
-        <Dialog open={isOpen} maxWidth="lg" onClose={handleClose} fullWidth>
-          <DialogTitle>
-            <AutoFocusTextField
-              InputComponent={OutlinedInput}
-              fullWidth
-              placeholder="Tìm kiếm sản phẩm ..."
-              value={keyWord}
-              onChange={handleTextChange}
-              onKeyPress={(e) => {
-                if (e.key === 'Enter') {
-                  handleNavigate();
-                }
-              }}
-            />
-          </DialogTitle>
-          <DialogContent sx={{ minHeight: '50vh' }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Grid container spacing={1.5}>
-                {results.map((product) => renderResultItem(product))}
-              </Grid>
-              {isLoading && <MCircularProgress size={48} sx={{ my: 3 }} />}
-              {renderSearchHistory()}
-            </Box>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} variant="contained">
-              Đóng
-            </Button>
-          </DialogActions>
-        </Dialog>
-
-        {/* <Slide direction="down" in={isOpen} mountOnEnter unmountOnExit>
-          <SearchBarStyle>
-            <Autocomplete
-              autoFocus
-              fullWidth
-              freeSolo
-              options={['dsdssd', 'sdds']}
-              placeholder={t('common.search-placeholder')}
-              startAdornment={
+      <Autocomplete
+        autoFocus
+        fullWidth
+        freeSolo
+        autoComplete
+        open={isOpen}
+        loading={isLoading}
+        loadingText="Đang tìm kiếm sản phẩm ...."
+        noOptionsText="Không có sản phẩm phù hợp."
+        options={results}
+        getOptionLabel={(option) => option.name || ''}
+        renderOption={(props, product) => renderResultItem(product)}
+        placeholder={t('common.search-placeholder')}
+        startAdornment={
+          <InputAdornment position="start">
+            <Box component={Icon} icon={searchFill} sx={{ color: 'text.disabled', width: 20, height: 20 }} />
+          </InputAdornment>
+        }
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            autoFocus
+            disableUnderline
+            placeholder={t('common.search-placeholder')}
+            variant="standard"
+            InputProps={{
+              ...params.InputProps,
+              startAdornment: (
                 <InputAdornment position="start">
-                  <Box component={Icon} icon={searchFill} sx={{ color: 'text.disabled', width: 20, height: 20 }} />
+                  <Box
+                    component={Icon}
+                    icon={searchFill}
+                    sx={{ color: 'text.disabled', width: 20, height: 20, zIndex: 999 }}
+                  />
                 </InputAdornment>
-              }
-              renderInput={(params) => (
-                <Input
-                  {...params}
-                  autoFocus
-                  fullWidth
-                  disableUnderline
-                  placeholder={t('common.search-placeholder')}
-                  startAdornment={
-                    <InputAdornment position="start">
-                      <Box component={Icon} icon={searchFill} sx={{ color: 'text.disabled', width: 20, height: 20 }} />
-                    </InputAdornment>
-                  }
-                />
-              )}
-              // value={keyWord}
-              onChange={handleTextChange}
-              onKeyPress={(e) => {
-                if (e.key === 'Enter') {
-                  handleNavigate();
-                }
-              }}
-              sx={{ mr: 1, fontWeight: 'fontWeightBold' }}
-            />
-            <Button style={{ textTransform: 'none' }} variant="contained" onClick={handleNavigate}>
-              {t('common.search-btn')}
-            </Button>
-          </SearchBarStyle>
-        </Slide> */}
-      </div>
+              )
+            }}
+            onClick={() => setOpen(true)}
+            onChange={handleTextChange}
+          />
+        )}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            handleNavigate();
+          }
+        }}
+        onKeyDown={(event) => {
+          if (event.key === 'Escape') {
+            setOpen(false);
+          }
+        }}
+        sx={{ mr: 1, fontWeight: 'fontWeightBold', maxWidth: 400, marginLeft: 3, zIndex: 999 }}
+      />
     </ClickAwayListener>
   );
 }
