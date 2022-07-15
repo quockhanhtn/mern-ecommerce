3fe07e3304b8ec297defc9f257a79a88f755842e
Khanh Lam <quockhanhdev@gmail.com>
2022-07-15 21:47:11 +0700

    Fixed order undefined

--- client/src/components/invoice/InvoicePDF.jsx
+++ client/src/components/invoice/InvoicePDF.jsx
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
 // ----------------------------------------------------------------------
 
 InvoicePDF.propTypes = {
-  invoice: PropTypes.object.isRequired
+  invoice: PropTypes.object
 };
 
 export default function InvoicePDF({ invoice }) {
--- client/src/utils/typeUtils.js
+++ client/src/utils/typeUtils.js
@@ -1,3 +1,11 @@
 export const getType = (object) => Object.prototype.toString.call(object).slice(8, -1);
 export const isFunction = (object) => getType(object) === 'Function';
 export const isString = (object) => getType(object) === 'String';
+
+export const isNotEmptyStr = (str) => {
+  if (str === undefined || str === null || !str) {
+    return false;
+  }
+
+  return isString(str) && str.trim().length > 0;
+};
