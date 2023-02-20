d31bad9cd4ce9badb423e5c0acac63870771e095
Khanh Lam <quockhanhdev@gmail.com>
2023-02-20 22:21:31 +0700

    Comment node-cron

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import cron from 'node-cron';
+//import cron from 'node-cron';
 import os from 'os';
 import { Server as SocketServer } from 'socket.io';
 
@@ -23,45 +23,45 @@ io.on('connection', (socket) => socketHandler(io, socket));
 serverApi.listen(serverPort, () => {
   LogUtils.info('SERVER', `Server running at ${serverIp}:${serverPort}`);
 
-  if (configs.isProd) {
-    let startMgs = '------------------------------------------------------';
-    startMgs += `\nServer running at *${serverIp}:${serverPort}*`;
-    const machineInfo = {
-      hostname: os.hostname(),
-      platform: os.platform(),
-      release: os.release(),
-      type: os.type(),
-      arch: os.arch(),
-      cpuModel: os.cpus()?.[0]?.model,
-      memory: `${FormatUtils.formatBytes(os.freemem())} / ${FormatUtils.formatBytes(os.totalmem())}`,
-    }
-    startMgs += `\nMachine info: \`\`\`${JSON.stringify(machineInfo, null, 2)}\`\`\``;
+  // if (configs.isProd) {
+  //   let startMgs = '------------------------------------------------------';
+  //   startMgs += `\nServer running at *${serverIp}:${serverPort}*`;
+  //   const machineInfo = {
+  //     hostname: os.hostname(),
+  //     platform: os.platform(),
+  //     release: os.release(),
+  //     type: os.type(),
+  //     arch: os.arch(),
+  //     cpuModel: os.cpus()?.[0]?.model,
+  //     memory: `${FormatUtils.formatBytes(os.freemem())} / ${FormatUtils.formatBytes(os.totalmem())}`,
+  //   }
+  //   startMgs += `\nMachine info: \`\`\`${JSON.stringify(machineInfo, null, 2)}\`\`\``;
 
-    startMgs += '\n------------------------------------------------------\n';
-    SlackUtils.sendMessage(startMgs);
+  //   startMgs += '\n------------------------------------------------------\n';
+  //   SlackUtils.sendMessage(startMgs);
 
-    const scheduleOpts = { scheduled: true, timezone: 'Asia/Ho_Chi_Minh' };
+  //   const scheduleOpts = { scheduled: true, timezone: 'Asia/Ho_Chi_Minh' };
 
-    // schedule task run every hour at minute 45
-    cron.schedule('20 * * * *', async () => {
-      SlackUtils.sendMessage('*[USER_BEHAVIOR]* Running a task every hour at minute 45 to import user behavior to FPT');
-      LogUtils.info('SERVER', 'Running a task every hour at minute 45 to import user behavior to FPT');
+  //   // schedule task run every hour at minute 45
+  //   cron.schedule('20 * * * *', async () => {
+  //     SlackUtils.sendMessage('*[USER_BEHAVIOR]* Running a task every hour at minute 45 to import user behavior to FPT');
+  //     LogUtils.info('SERVER', 'Running a task every hour at minute 45 to import user behavior to FPT');
 
-      fptService.importUserBehaviorToFpt();
-    }, scheduleOpts);
+  //     fptService.importUserBehaviorToFpt();
+  //   }, scheduleOpts);
 
-    // schedule task run every day at 02:00 AM import product data to FPT
-    cron.schedule('0 2 * * *', () => {
-      LogUtils.info('SERVER', 'Running a task every day at 02:00 AM to import product data to FPT');
-      SlackUtils.sendMessage('*[RECOMMEND_IMPORT]* Running a task every day at 03:00 AM to product import data to FPT');
-      fptService.importProductDataToFpt()
-    }, scheduleOpts);
+  //   // schedule task run every day at 02:00 AM import product data to FPT
+  //   cron.schedule('0 2 * * *', () => {
+  //     LogUtils.info('SERVER', 'Running a task every day at 02:00 AM to import product data to FPT');
+  //     SlackUtils.sendMessage('*[RECOMMEND_IMPORT]* Running a task every day at 03:00 AM to product import data to FPT');
+  //     fptService.importProductDataToFpt()
+  //   }, scheduleOpts);
 
-    // schedule task run every day at 04:00 AM to update recommend data
-    cron.schedule('0 4 * * *', () => {
-      LogUtils.info('SERVER', 'Running a task every day at 04:00 AM to to update recommend data');
-      SlackUtils.sendMessage('*[RECOMMEND_UPDATE]* Running a task every day at 04:00 AM to update recommend data');
-      fptService.updateRecommendData()
-    }, scheduleOpts);
-  }
+  //   // schedule task run every day at 04:00 AM to update recommend data
+  //   cron.schedule('0 4 * * *', () => {
+  //     LogUtils.info('SERVER', 'Running a task every day at 04:00 AM to to update recommend data');
+  //     SlackUtils.sendMessage('*[RECOMMEND_UPDATE]* Running a task every day at 04:00 AM to update recommend data');
+  //     fptService.updateRecommendData()
+  //   }, scheduleOpts);
+  // }
 });
